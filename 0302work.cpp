5cad07b6f3e276e3f9c8927e9a985843239ef991
A112510330 <151697820+A112510330@users.noreply.github.com>
2026-03-02 10:11:50 +0800

    Add files via upload

---
+++ 0302work.cpp
@@ -0,0 +1,22 @@
+
+
+// Version: 0.1
+#include <iostream>
+using namespace std;
+
+int main()
+
+int level = 5;
+int score = 80;
+char grand = 'B';
+
+cout << "年級\t" << “得分\t" << “等級” << endl;
+cout << level << "\t" << score << "\t" << grand << endl;
+
+level = 6;
+score = 91;
+grand = 'A';
+
+cout << level << "\t" << score << "\t" << grand << endl;
+
+return 0;
